238a230a298a678a44e30fccbe3d4f4db110f4b3
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-08 11:24:03 -0400

    renamed function

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,4 +1,4 @@
-async function getProduct(id:number) {
+async function getProductById(id:number) {
     try {
         const response = await fetch(`https://dummyjson.com/products/${id}`)
         const data = await response.json();
