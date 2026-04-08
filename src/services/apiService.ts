e1c460282e546cb716b2665595490551106f6008
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-08 13:28:05 -0400

    added custom errors in async function

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,10 +1,19 @@
+import { CustomError } from "../utils/errorHandler.js";
+
 async function getProductById(id:number) {
     try {
         const response = await fetch(`https://dummyjson.com/products/${id}`)
+        if(!response) {
+            throw new CustomError("Product not found", "APIError");
+        }
         const data = await response.json();
         console.log(data)
         return data
-    } catch(error) {
-        console.error("Error message: ", error);
+    } catch(error: any) {
+        if(error instanceof CustomError) {
+            throw error;
+        } else {
+            throw new CustomError(error.message, "NetworkError")
+        }
     }
 }
