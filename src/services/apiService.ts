5a9dad44c0f19cb4c95955101ff0b9ee2a374946
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-08 15:41:11 -0400

    formatted file

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -3,14 +3,14 @@ import { CustomError } from "../utils/errorHandler.js";
 export async function getProduct() {
     try {
         const response = await fetch(`https://dummyjson.com/products/1`)
-        if(!response.ok) {
+        if (!response.ok) {
             throw new CustomError("Product not found", "APIError");
         }
         const data = await response.json();
         // console.log(data)
         return data
-    } catch(error: any) {
-        if(error instanceof CustomError) {
+    } catch (error: any) {
+        if (error instanceof CustomError) {
             throw error;
         } else {
             throw new CustomError(error.message, "NetworkError")
