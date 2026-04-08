2ec535bbfd47a824412710ea1c74dc18249d22fb
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-08 14:21:18 -0400

    updated import for apiService

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { Product } from "./models/Product.ts";
 import { calculateTax } from "./utils/taxCalculator.ts";
-import { getProductById } from "./services/apiService.ts";
+import { getProduct } from "./services/apiService.ts";
 
